9c2b056473d28ef22767b5c5c7105a05c65c97d9
michal_cfc <michal.jachimowski@gmail.com>
2022-07-05 12:14:03 +0200

    init project

--- src/layout/Nav/Nav.styles.tsx
+++ src/layout/Nav/Nav.styles.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from '@components/Link';
+import { Link } from '~components/common/Link';
 import breakpoint from '~utils/styles/breakpoints';
 
 import { space, SpaceProps } from 'styled-system';

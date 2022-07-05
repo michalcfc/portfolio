8e7a9abf594fe6787b160aa7b6fd1b7ad9e5aec7
michal_cfc <michal.jachimowski@gmail.com>
2022-07-05 15:50:51 +0200

    init project

--- src/layout/Header/components/MobileMenu/MobileMenu.tsx
+++ src/layout/Header/components/MobileMenu/MobileMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 import { motion, useCycle } from 'framer-motion';
 import { MobileMenuD } from './MobileMenu.d';
